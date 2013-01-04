c6452f2a13083aeba29739e2cd5f126c1277ecd7
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-01-04 23:39:27 +0100

    Tweak NS manager

--- public/aceeditor/javascripts/namespace.js
+++ public/aceeditor/javascripts/namespace.js
@@ -2,6 +2,15 @@ var Namespace = Namespace || {};
 
 (function(exports) {
 
+    if (typeof exports == "undefined") {
+        console.error("The namespace doesn't exists.");
+        return;
+    }
+    if (typeof Namespace == "undefined") {
+        console.error("The namespace Namespace doesn't exists.");
+        return;
+    }
+
     exports.lookup = function(namespaceString) {
         var parts = namespaceString.split('.');
         var parent = window;
@@ -18,4 +27,27 @@ var Namespace = Namespace || {};
        callback(exports.lookup(namespace));
     };
 
+    exports.define = function(namespace, callback) {
+        callback(exports.lookup(namespace));
+    };
+
 })(Namespace);
+
+/**
+ *
+ * Usage of namespace lib :
+ *
+ * Namespace.define('play.foo.bar', function(exports) {
+ *     exports.hello = function(name) {
+ *         console.log("hello %s!", name);
+ *     };
+ * });
+ *
+ * Namespace.use('play.foo.bar', function(NS) {
+ *     NS.hello('foobar');
+ * });
+ *
+ * var NS = NS || Namespace.lookup('play.foo.bar');
+ * NS.hello('foobar');
+ *
+ **/
