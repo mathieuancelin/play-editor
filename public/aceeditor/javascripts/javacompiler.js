2cc46d7d30940250b58548e366a6e6e10e5642ca
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-01-04 22:44:48 +0100

    Modularize JavaCompiler

---
+++ public/aceeditor/javascripts/javacompiler.js
@@ -0,0 +1,17 @@
+var JavaCompiler = {};
+(function(exports) {
+
+    exports.compile = function(src) {
+        return $.post('/@editor/compile', {src: src, path: $('#currentFileValue').html()}, function(data) {
+            if (!data.compilation) {
+                var line = data.errorLine - 1;
+                editor.getSession().setAnnotations([{text: data.msg, row: line, type:"error"}]);
+                $('#message').attr('class', 'label label-important');
+            } else {
+                editor.getSession().setAnnotations([]);
+                $('#message').attr('class', 'label label-success');
+            }
+        });
+    };
+
+})(JavaCompiler);
