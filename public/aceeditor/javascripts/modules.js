315f407996db3c245f6adefb04e0eadcd5589356
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-01-05 14:29:47 +0100

    Add API documentation

--- public/aceeditor/javascripts/modules.js
+++ public/aceeditor/javascripts/modules.js
@@ -10,7 +10,7 @@ var CommonUtils = CommonUtils || {};
                 this.items[p] = obj[p];
                 this.length++;
             }
-        };
+        }
         this.put = function(key, value) {
             var previous = undefined;
             if (this.containsKey(key)) {
@@ -75,31 +75,41 @@ var Modules = Modules || {};
 (function(exports) {
 
     if (typeof exports == "undefined") {
-        throw "The namespace doesn't exists.";
+        throw "The passed namespace isn't defined.";
     }
+    // check if CommonUtils is defined to use Map()
     if (typeof CommonUtils == "undefined") {
-        throw "The namespace CommonUtils doesn't exists.";
+        throw "The namespace 'CommonUtils' doesn't exists.";
     }
+    // check if Modules is defined to be used by others
     if (typeof Modules == "undefined") {
-        throw "The namespace Modules doesn't exists.";
+        throw "The namespace 'Modules' doesn't exists.";
     }
 
+    /**
+     * Map of (identifier, module)
+     *
+     * @type {CommonUtils.Map}
+     */
     exports.modules = new CommonUtils.Map();
 
+    /**
+     * Create a new module for identifier or get the existing one.
+     * Add some functions and members to the module :
+     * - moduleIdentifier
+     * - moduleName extracted from moduleIdentifier
+     * - moduleVersion extracted from moduleIdentifier
+     * - setupModule()
+     * - moduleReady()
+     * - messageReceived(msg)
+     *
+     * @param namespaceString identifier of the module
+     * @return {*} the created or existing module
+     */
     exports.lookup = function(namespaceString) {
-        /**
-        var parts = namespaceString.split('.');
-        var parent = window;
-        var currentPart = '';
-        for(var i = 0, length = parts.length; i < length; i++) {
-            currentPart = parts[i];
-            parent[currentPart] = parent[currentPart] || {};
-            parent = parent[currentPart];
-        }
-        return parent;
-        **/
         if (!exports.modules.containsKey(namespaceString)) {
             var newModule = {};
+            newModule.moduleIdentifier = namespaceString;
             var parts = namespaceString.split(':');
             if (parts.length > 1) {
                 newModule.moduleName = parts[0];
@@ -119,6 +129,13 @@ var Modules = Modules || {};
         return exports.modules.get(namespaceString);
     };
 
+    /**
+     * Get the required Module if exists and apply callback on it.
+     *
+     * @param namespace module identifier
+     * @param callback function that will use the required module (passed as first argument)
+     * @return {*} callback call result
+     */
     exports.use = function(namespace, callback) {
        if (!exports.modules.containsKey(namespace)) {
            throw ("Module " + namespace + " doesn't exists.");
@@ -126,6 +143,13 @@ var Modules = Modules || {};
        return callback(exports.lookup(namespace));
     };
 
+    /**
+     * Get the required modules if they exists and apply callback on them.
+     *
+     * @param namespaces array of module identifiers
+     * @param callback function that will use the required modules (passed as arguments)
+     * @return {*} callback call result
+     */
     exports.uses = function(namespaces, callback) {
         var dependencies = [];
         namespaces.forEach(function(item, idx, array) {
@@ -137,6 +161,13 @@ var Modules = Modules || {};
         return callback.apply(null, dependencies);
     };
 
+    /**
+     * Define a new Module if it doesn't exists
+     *
+     * @param namespace module identifier
+     * @param callback function that will define module structure
+     * @return {*} callback call result
+     */
     exports.define = function(namespace, callback) {
         if (exports.modules.containsKey(namespace)) {
             throw ("Module " + namespace + " already exists.");
@@ -146,6 +177,14 @@ var Modules = Modules || {};
         return mod;
     };
 
+    /**
+     * Define a new Module if it doesn't exists and requires dependencies on other modules.
+     *
+     * @param namespace module identifier
+     * @param deps array of module identifiers
+     * @param callback callback function that will define module structure
+     * @return {*} callback call result
+     */
     exports.defineWithDependencies = function(namespace, deps, callback) {
         if (exports.modules.containsKey(namespace)) {
             throw ("Module " + namespace + " already exists.");
@@ -163,23 +202,36 @@ var Modules = Modules || {};
         return mod;
     };
 
+    /**
+     * Function that can be called when the environment is ready to work.
+     * Call 'setupModule()' on each existing module.
+     */
     exports.modulesReady = function() {
         exports.modules.each(function(idx, item) {
             item.moduleReady();
         });
     };
 
+    /**
+     * Call 'moduleReady()' on each existing module.
+     */
     exports.modulesSetup = function() {
         exports.modules.each(function(idx, item) {
             item.setupModule();
         });
     };
 
+    /**
+     * Init the module system. For instance in $(document).ready(Modules.initModules)
+     */
     exports.initModules = function() {
         exports.modulesSetup();
         exports.modulesReady();
     };
 
+    /**
+     * Print the current status of module system
+     */
     exports.printModules = function() {
         console.log("\n=========================================\n");
         console.log("Available modules are : \n");
@@ -189,32 +241,68 @@ var Modules = Modules || {};
         console.log("\n=========================================\n");
     };
 
+    /**
+     * Call 'messageReceived(msg)' on each registered module
+     *
+     * @param msg broadcasted message
+     */
     exports.broadcast = function(msg) {
         exports.modules.each(function(idx, item) {
             item.messageReceived(msg);
         });
     };
 
+    /**
+     * Call 'messageReceived(msg)' on specified modules
+     *
+     * @param modules array of module identifiers
+     * @param msg sent message
+     */
     exports.sendToModules = function(modules, msg) {
         modules.forEach(function(item, idx, array) {
             exports.modules.each(function(modIdx, mod) {
-                if (mod.moduleName == item) {
+                if (mod.moduleIdentifier == item) {
                     mod.messageReceived(msg);
                 }
             });
         });
     };
 
+    /**
+     * Call 'messageReceived(msg)' on modules with matching identifiers
+     *
+     * @param regex regular expression that matches module identifier
+     * @param msg sent message
+     */
     exports.sendToModulesMatching = function(regex, msg) {
         exports.modules.each(function(modIdx, mod) {
-            if (mod.moduleName.match(regex)) {
+            if (mod.moduleIdentifier.match(regex)) {
                 mod.messageReceived(msg);
             }
         });
     };
 
-    exports.sendToModule = function(moduleName, msg) {
-         exports.lookup(moduleName).messageReceived(msg);
+    /**
+     * Call 'messageReceived(msg)' on specified module
+     *
+     * @param moduleIdentifier identifier of the module
+     * @param msg sent message
+     */
+    exports.sendToModule = function(moduleIdentifier, msg) {
+         exports.lookup(moduleIdentifier).messageReceived(msg);
+    };
+
+    /**
+     * Remove an existing module from module system.
+     *
+     * @param moduleIdentifier the module identifier
+     * @return {*} the removed module
+     */
+    exports.remove = function(moduleIdentifier) {
+        if (!exports.modules.containsKey(moduleIdentifier)) {
+            throw ("Module " + moduleIdentifier + " doesn't exists.");
+        }
+        return exports.modules.remove(moduleIdentifier);
     };
 
 })(Modules);
@@ -234,6 +322,116 @@ var Modules = Modules || {};
  * });
  *
  * var Bar = Bar || Modules.lookup('play.foo.bar');
+ *
  * Bar.hello('foobar');
  *
+ * Modules.define('ModuleA', function(ModuleA) {
+ *     ModuleA.hello = function() {
+ *         console.log("Hello from module A");
+ *     };
+ *     ModuleA.setupModule = function() {
+ *         console.log("Setup ModuleA");
+ *     };
+ *     ModuleA.messageReceived = function(msg) {
+ *         console.log("Received in ModuleA %s", msg);
+ *     };
+ * });
+ *
+ * Modules.define('ModuleA:1.0', function(ModuleA) {
+ *     ModuleA.hello = function() {
+ *         console.log("Hello from module A v 1.0");
+ *     };
+ *     ModuleA.setupModule = function() {
+ *         console.log("Setup ModuleA v 1.0");
+ *     };
+ *     ModuleA.messageReceived = function(msg) {
+ *         console.log("Received in ModuleA v 1.0 %s", msg);
+ *     };
+ * });
+ *
+ * Modules.define('ModuleA:2.0', function(ModuleA) {
+ *     ModuleA.hello = function() {
+ *         console.log("Hello from module A v 2.0");
+ *     };
+ *     ModuleA.setupModule = function() {
+ *         console.log("Setup ModuleA v 2.0");
+ *     };
+ *     ModuleA.messageReceived = function(msg) {
+ *         console.log("Received in ModuleA v 2.0 %s", msg);
+ *     };
+ * });
+ *
+ * Modules.define('ModuleB', function(ModuleB) {
+ *     ModuleB.hello = function() {
+ *         console.log("Hello from module B");
+ *     };
+ *     ModuleB.moduleReady = function() {
+ *         console.log("ModuleB is ready !!!");
+ *     };
+ *     ModuleB.messageReceived = function(msg) {
+ *         console.log("Received in ModuleB %s", msg);
+ *     };
+ * });
+ *
+ * Modules.defineWithDependencies('ModuleC', ['ModuleA', 'ModuleB'], function(ModuleC, ModuleA, ModuleB) {
+ *     ModuleC.hello = function() {
+ *         ModuleA.hello();
+ *         ModuleB.hello();
+ *         console.log("Hello from module C");
+ *     };
+ *     ModuleC.messageReceived = function(msg) {
+ *         console.log("Received in ModuleC %s", msg);
+ *     };
+ * });
+ *
+ * Modules.defineWithDependencies('ModuleD', ['ModuleC'], function(ModuleD, ModuleC) {
+ *     ModuleD.hello = function() {
+ *         ModuleC.hello();
+ *         console.log("Hello from module D");
+ *     };
+ *     ModuleD.messageReceived = function(msg) {
+ *          console.log("Received in ModuleD %s", msg);
+ *     };
+ * });
+ *
+ * Modules.initModules();
+ * Modules.printModules();
+ * Modules.use('ModuleD', function(ModuleD) {
+ *     ModuleD.hello();
+ *     console.log('#####################################')
+ * });
+ * Modules.uses(['ModuleA', 'ModuleB', 'ModuleC', 'ModuleD'], function(ModuleA, ModuleB, ModuleC, ModuleD) {
+ *     ModuleA.hello();
+ *     ModuleB.hello();
+ *     ModuleC.hello();
+ *     ModuleD.hello();
+ * });
+ *
+ * Modules.broadcast("Hello Modules ...");
+ *
+ * Modules.sendToModule('ModuleA', 'Hello ModuleA ...');
+ *
+ * Modules.sendToModules(['ModuleA', 'ModuleB'], 'Hello ModuleA and ModuleB ...');
+ *
+ * Modules.sendToModulesMatching(/Module[A-B]/i, 'Hello Module matching AB...');
+ * Modules.sendToModulesMatching(/Module[C-D]/i, 'Hello Module matching CD...');
+ * Modules.sendToModulesMatching(/Module[A-Z]:[0-9*]\.[0-9*]/i, 'Hello versioned Modules ...');
+ * Modules.sendToModulesMatching(/Module[A-z]:1\.[0-9*]/i, 'Hello Modules in v 1.x ...');
+ * Modules.sendToModulesMatching(/Module[A-Z]:2\.[0-9*]/i, 'Hello Modules in v 2.x ...');
+ *
+ **/
+
+
+
+// OLD CODE
+/**
+ var parts = namespaceString.split('.');
+ var parent = window;
+ var currentPart = '';
+ for(var i = 0, length = parts.length; i < length; i++) {
+            currentPart = parts[i];
+            parent[currentPart] = parent[currentPart] || {};
+            parent = parent[currentPart];
+        }
+ return parent;
  **/
